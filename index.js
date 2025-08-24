22b0f06e2d96ed625e9c9c29d81cca05ce339c9a
mayureebudhe <budhemayuree@gmail.com>
2025-08-24 23:02:31 +0530

    feat: Setup template engine, EJS, for SSR

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import path from "path";
 import { connectMongoDB } from "./connect.js";
 import { urlRouter } from "./routes/url.js";
-
+import { staticRouter } from "./routes/staticRouter.js";
 const app = express();
 const PORT = 8001;
 
 await connectMongoDB("mongodb://127.0.0.1:27017/short-url");
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
 app.use("/api/url", urlRouter);
+app.use("/", staticRouter);
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
